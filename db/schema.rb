5c62f5db46074470d0ccccaf4e77868ef940e2f3
ornerymoose <dpardy87@gmail.com>
2015-12-03 13:50:17 -0500

    fixed form argument, and category_ids had to be plural

--- db/schema.rb
+++ db/schema.rb
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(version: 20151203162008) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "name"
-    t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
