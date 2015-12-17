22dabbecbaaac98130796bf29ee2483a44cf7091
ornerymoose <dpardy87@gmail.com>
2015-12-17 14:03:20 -0500

    added id to admin panel for users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,9 +2,10 @@ ActiveAdmin.register User do
  
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  permit_params :email, :role
+  permit_params :id, :email, :role
  
     index do
+        column :id
         column :email
         column :current_sign_in_at
         column :last_sign_in_at
