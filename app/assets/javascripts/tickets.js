9e44edd333e1eae2cf7f23e39fd0d84bac773673
ornerymoose <dpardy87@gmail.com>
2016-01-20 13:40:12 -0500

    updated text field validation for ticket creation

--- app/assets/javascripts/tickets.js
+++ app/assets/javascripts/tickets.js
@@ -11,6 +11,20 @@ function validate(evt) {
 
 $(function() {
   initPage();
+  $('.ticket-submit').attr("disabled", "disabled");
+  $('.htn, .additional_notes, .problem_statement').keyup(function() {
+        var empty = false;        
+        var htn_length = $(".htn").val().length;
+        var an_length = $(".additional_notes").val().length;
+        var ps_length = $(".problem_statement").val().length;
+        var total_length = htn_length + an_length + ps_length;
+        if (total_length <= 10) {
+            empty = true;
+            $('.ticket-submit').prop( "disabled", true );
+        } else {
+            $('.ticket-submit').prop( "disabled", false );
+        }
+    });
 });
 $(window).bind('page:change', function() {
   initPage();
