b1377d8831ff0118f6747fa1fc0fbce4a9911b5e
ornerymoose <dpardy87@gmail.com>
2016-01-27 16:30:47 -0500

    cleaned up ticket creation validation

--- app/assets/javascripts/tickets.js
+++ app/assets/javascripts/tickets.js
@@ -12,19 +12,14 @@ function validate(evt) {
 $(function() {
   initPage();
   $('.ticket-submit').attr("disabled", "disabled");
-  $('.htn, .additional_notes, .problem_statement').keyup(function() {
-        var empty = false;        
-        var htn_length = $(".htn").val().length;
-        var an_length = $(".additional_notes").val().length;
-        var ps_length = $(".problem_statement").val().length;
-        var total_length = htn_length + an_length + ps_length;
-        if (total_length <= 10) {
-            empty = true;
-            $('.ticket-submit').prop( "disabled", true );
+  $('.htn, .additional_notes, .problem_statement').keyup(function(){
+        if( ($('.htn').val().length !=0) && ($('.additional_notes').val().length !=0) && ($('.problem_statement').val().length !=0) ) {
+          $('.ticket-submit').prop( "disabled", false );
         } else {
-            $('.ticket-submit').prop( "disabled", false );
+          $('.ticket-submit').prop( "disabled", true );
         }
     });
+
 });
 $(window).bind('page:change', function() {
   initPage();
