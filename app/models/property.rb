f7399e823b0706d482790430997dae6debb3854a
ornerymoose <dpardy87@gmail.com>
2016-02-17 10:29:57 -0500

    updated property form and model validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ActiveRecord::Base
 	belongs_to :category
-
+	validates_presence_of :name, :address, :latitude, :longitude, :category_id, :on => :create
 	#has_many :categorizations
 	#has_many :categories, through: :categorizations
 	def property_name_and_address
