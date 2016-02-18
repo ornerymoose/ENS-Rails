874386e83ee3ae3af38a79e424c1c8541b80d7c8
ornerymoose <dpardy87@gmail.com>
2016-02-18 15:31:06 -0500

    removed js validation for subs and tickets

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,4 +4,5 @@ class Subscription < ActiveRecord::Base
 
 	belongs_to :user
 	validates_presence_of :categories, :on => :create
+	validates :phone_number, numericality: true, length: { minimum: 10 }
 end
