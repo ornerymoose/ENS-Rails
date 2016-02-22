6c18ace9f602b9408bfb77a08de0aa2f0b794257
ornerymoose <dpardy87@gmail.com>
2016-02-22 12:38:29 -0500

    updated phone number validation on subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,6 +4,8 @@ class Subscription < ActiveRecord::Base
 
 	belongs_to :user
 	validates_presence_of :categories, :on => :create
-	validates :phone_number, phony_plausible: true, numericality: true, :allow_blank => true, length: { minimum: 10 }
+
+	validates_numericality_of :phone_number, :allow_nil => true,  :message => "has to be numbers only"
+	validates :phone_number, phony_plausible: true, numericality: {only_integer: true}, :allow_blank => true, length: { minimum: 10 }
 	phony_normalize :phone_number, default_country_code: 'US'
 end
