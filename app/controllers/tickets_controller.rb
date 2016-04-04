9df4706a7f2fccf8d10ac7656c557b2c5df540d9
ornerymoose <dpardy87@gmail.com>
2016-04-04 10:34:24 -0400

    removed iteration for tickets#update on email sending as it is unncessary

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -136,9 +136,9 @@ class TicketsController < ApplicationController
                 #code below for emails
                 @people_for_email = @sub_emails.select {|user| user["categories"].include?(@ticket_category)}
                 @emails_for_email = @people_for_email.map {|emails| emails["name"]}
-                @emails_for_email.each do |email|
-                    UserNotifier.ticket_updated(email, @property_array, @ticket.heat_ticket_number, @ticket.bridge_number, @ticket.customers_affected, @ticket_category, @ticket.event_category, @ticket.event_severity, @ticket.event_status, @ticket.created_at, @ticket.problem_statement, @ticket.additional_notes, @ticket.attachment, @ticket.versions).deliver_now                
-                end
+                #@emails_for_email.each do |email|
+                    UserNotifier.ticket_updated(@emails_for_email, @property_array, @ticket.heat_ticket_number, @ticket.bridge_number, @ticket.customers_affected, @ticket_category, @ticket.event_category, @ticket.event_severity, @ticket.event_status, @ticket.created_at, @ticket.problem_statement, @ticket.additional_notes, @ticket.attachment, @ticket.versions).deliver_now                
+                #end
 
                 #code below for SMS        
                 @people_for = @people.select {|user| user["categories"].include?(@ticket_category)}
