a8b1a5dd05f65caf41d913ad3c7f2aafbab3979b
ornerymoose <dpardy87@gmail.com>
2016-04-11 13:12:51 -0400

    removed commented out code from property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,11 +2,7 @@ class Property < ActiveRecord::Base
 	belongs_to :category
 	validates_presence_of :name, :address, :latitude, :longitude, :category_id, :on => :create
 
-	# scope :alphabetically, ->{
-	# 	:order "name DESC"
-	# }
 	default_scope { order('name asc') }
-
 
 	def property_name_and_address
 		if self.address == ""
